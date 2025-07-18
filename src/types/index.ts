66597f4e7fc59af971c498b6828a3aa51a06a192
Barbara <95110916+BarbaraMartel@users.noreply.github.com>
2025-07-18 02:43:22 -0500

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -6,15 +6,22 @@ export type User = {
     description: string
     image: string
     links: string
+    visits?: number // 👈 Añadido opcionalmente por si aún no existe en algunos usuarios
 }
-export type UserHandle = Pick<User, 'description' | 'handle' | 'image' |'links' | 'name'>
+
+export type UserHandle = Pick<User, 'description' | 'handle' | 'image' | 'links' | 'name'> & {
+    visits: number // 👈 Añadido aquí explícitamente
+}
+
 export type RegisterForm = Pick<User, 'handle' | 'email' | 'name'> & {
     password: string
     password_confirmation: string
 }
+
 export type LoginForm = Pick<User, 'email'> & {
     password: string
 }
+
 export type ProfileForm = Pick<User, 'handle' | 'description'>
 
 export type SocialNetwork = {
@@ -23,4 +30,5 @@ export type SocialNetwork = {
     url: string
     enabled: boolean
 }
+
 export type DevTreeLink = Pick<SocialNetwork, 'name' | 'url' | 'enabled'>
